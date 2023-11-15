d51d2d9cf650d68b57bb68b852114e596fba9e14
Emanuel Naissinger <64567310+Naissinger@users.noreply.github.com>
2023-11-15 20:54:09 -0300

    Update

--- Bot/calculateMove.py
+++ Bot/calculateMove.py
@@ -1,55 +1,52 @@
 import sys
 from stockfish import Stockfish
 
-stockfish = Stockfish("stockfish15\\stockfish_15_x64_avx2.exe")
+stockfish = Stockfish("stockfish16\\stockfish-windows-x86-64-avx2.exe")
 
 stockfish.set_skill_level(20)
 
 entrada = sys.argv[1::]
 
-if(entrada[0] == "-f"):
-    
+if entrada[0] == "-f":
     try:
-        if(entrada[2] == "-b"):
+        if entrada[2] == "-b":
             entradaSplit = entrada[1].split(" b")
             fen = entradaSplit[0][::-1]
-            fen = fen + ' b' + entradaSplit[1]
+            fen = fen + " b" + entradaSplit[1]
 
             print(fen)
             stockfish.set_fen_position(fen)
 
-            print(f'\nBest Move: {stockfish.get_best_move_time(100)}\n')
-            print(f'Mate em: {stockfish.get_evaluation()}')
+            print(f"\nBest Move: {stockfish.get_best_move()}\n")
+            print(f"Mate em: {stockfish.get_evaluation()}")
             print(stockfish.get_board_visual())
         else:
             stockfish.set_fen_position(entrada[1])
 
-            move = stockfish.get_best_move_time(100)
-            
-            print(f'\nBest Move: {move}\n')
-            
+            move = stockfish.get_best_move()
+
+            print(f"\nBest Move: {move}\n")
+
             stockfish.make_moves_from_current_position([move])
 
-            print(f'Mate em: {stockfish.get_evaluation()}')
-            print(f'FEN: {stockfish.get_fen_position()}')
+            print(f"Mate em: {stockfish.get_evaluation()}")
+            print(f"FEN: {stockfish.get_fen_position()}")
             print(stockfish.get_board_visual())
     except:
         stockfish.set_fen_position(entrada[1])
-        move = stockfish.get_best_move_time(100)
-        
-        print(f'\nBest Move: {move}\n')
+        move = stockfish.get_best_move()
+
+        print(f"\nBest Move: {move}\n")
 
         stockfish.make_moves_from_current_position([move])
 
-        print(f'Mate em: {stockfish.get_evaluation()}')
-        print(f'FEN: {stockfish.get_fen_position()}')
+        print(f"Mate em: {stockfish.get_evaluation()}")
+        print(f"FEN: {stockfish.get_fen_position()}")
         print(stockfish.get_board_visual())
-elif(entrada[0] == "-m"):
-
+elif entrada[0] == "-m":
     stockfish.set_fen_position(entrada[3])
-    
+
     stockfish.make_moves_from_current_position([entrada[1]])
 
-    print(f'FEN: {stockfish.get_fen_position()}')
+    print(f"FEN: {stockfish.get_fen_position()}")
     print(stockfish.get_board_visual())
-
